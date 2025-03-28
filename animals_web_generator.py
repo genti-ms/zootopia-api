b8380cbe669441e6fec90f3e80a933344eca1f10
genti-ms <genti.ms10@icloud.com>
2025-03-28 19:50:26 +0100

    Final: access data from json file. Had found an mistake because of that final

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,43 +1,36 @@
 import json
 
 
-# Function to load the JSON file
 def load_data(file_path):
-    """ Loads a JSON file """
+    """Lädt eine JSON-Datei"""
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
-# Load the data from the JSON file
-animals_data = load_data('animals_data.json')
+def print_animal_data(animals):
+    """Druckt die relevanten Daten für jedes Tier"""
+    for animal in animals:
+        if "name" in animal:
+            print(f"Name: {animal['name']}")
 
-# Read the content of the template HTML file
-with open('animals_template.html', 'r') as template_file:
-    template_content = template_file.read()
+        if "characteristics" in animal:
+            characteristics = animal["characteristics"]
 
-# Generate an HTML list with the animals' data
-output = '<ul class="cards">\n'  # Start the unordered list
-for animal in animals_data:
-    output += '<li class="cards__item">\n'
-    output += f"Name: {animal['name']}<br/>\n"
+            if "diet" in characteristics:
+                print(f"Diet: {characteristics['diet']}")
 
-    if 'characteristics' in animal:
-        if 'diet' in animal['characteristics']:
-            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-        if 'type' in animal['characteristics']:
-            output += f"Type: {animal['characteristics']['type']}<br/>\n"
+            if "locations" in animal and animal["locations"]:
+                print(f"Location: {animal['locations'][0]}")
 
-    if 'locations' in animal and animal['locations']:
-        output += f"Location: {animal['locations'][0]}<br/>\n"
+            if "type" in characteristics:
+                print(f"Type: {characteristics['type']}")
 
-    output += '</li>\n'
-output += '</ul>'  # Close the unordered list
+        print()
 
-# Replace placeholder in the template with the generated HTML
-final_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
-# Write the new HTML content to a file
-with open('animals.html', 'w') as output_file:
-    output_file.write(final_content)
+if __name__ == "__main__":
+    # Lade die Daten aus der JSON-Datei
+    animals_data = load_data("animals_data.json")
 
-print("HTML file generated successfully as 'animals.html'.")
+    # Drucke die relevanten Informationen
+    print_animal_data(animals_data)
