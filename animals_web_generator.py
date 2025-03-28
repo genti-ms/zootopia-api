03a86959b444fa182adc3b3386bb0b8bda6734c4
genti-ms <genti.ms10@icloud.com>
2025-03-28 19:40:31 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,31 +11,32 @@ def load_data(file_path):
 # Load the data from the JSON file
 animals_data = load_data('animals_data.json')
 
-# Step 1: Read the content of the template HTML file
+# Read the content of the template HTML file
 with open('animals_template.html', 'r') as template_file:
     template_content = template_file.read()
 
-# Step 2: Generate a string with the animals' data
-output = ''  # Define an empty string
+# Generate an HTML list with the animals' data
+output = '<ul class="cards">\n'  # Start the unordered list
 for animal in animals_data:
-    # Start with the Name
-    output += f"Name: {animal['name']}\n"
+    output += '<li class="cards__item">\n'
+    output += f"Name: {animal['name']}<br/>\n"
 
     if 'characteristics' in animal:
         if 'diet' in animal['characteristics']:
-            output += f"Diet: {animal['characteristics']['diet']}\n"
+            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
         if 'type' in animal['characteristics']:
-            output += f"Type: {animal['characteristics']['type']}\n"
+            output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-    # Print Location (first location in the list)
     if 'locations' in animal and animal['locations']:
-        output += f"Location: {animal['locations'][0]}\n"
+        output += f"Location: {animal['locations'][0]}<br/>\n"
 
+    output += '</li>\n'
+output += '</ul>'  # Close the unordered list
 
-# Step 3: Replace __REPLACE_ANIMALS_INFO__ in the template with the output
+# Replace placeholder in the template with the generated HTML
 final_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
-# Step 4: Write the new HTML content to a new file
+# Write the new HTML content to a file
 with open('animals.html', 'w') as output_file:
     output_file.write(final_content)
 
